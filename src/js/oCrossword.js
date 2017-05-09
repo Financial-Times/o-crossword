515d8b6acafff814ed775adfb72278d2dbb9803d
Lily2point0 <lily.madar@ft.com>
2017-05-09 10:15:45 +0100

    implemented grid focus from clue list

--- src/js/oCrossword.js
+++ src/js/oCrossword.js
@@ -266,6 +266,8 @@ OCrossword.prototype.assemble = function assemble() {
 		magicInput.type = 'text';
 		magicInput.style.display = 'none';
 
+		let blockHighlight = false;
+
 		this.hammerMC = new Hammer.Manager(this.rootEl, {
 			recognizers: [
 				[Hammer.Tap]
@@ -329,6 +331,7 @@ OCrossword.prototype.assemble = function assemble() {
 			magicInput.value = '';
 			magicInput.blur();
 			magicInput.style.display = 'none';
+			blockHighlight = false;
 		}, 16);
 		this.addEventListener(magicInput, 'focus', magicInput.select());
 
@@ -472,6 +475,8 @@ OCrossword.prototype.assemble = function assemble() {
 		}.bind(this));
 
 		function highlightGridByCluesEl(el) {
+			if(blockHighlight) return;
+
 			while(el.parentNode) {
 				if (el.dataset.oCrosswordNumber) {
 					highlightGridByNumber(Number(el.dataset.oCrosswordNumber), el.dataset.oCrosswordDirection, el.dataset.oCrosswordAnswerLength);
@@ -504,22 +509,72 @@ OCrossword.prototype.assemble = function assemble() {
 			gridElsToHighlight.forEach(el => el.dataset.oCrosswordHighlighted = direction);
 		}
 
+		function unsetClue(number, direction, length) {
+			const el = cluesEl.querySelector(`li[data-o-crossword-number="${number}"][data-o-crossword-direction="${direction}"]`);
+			if (el) {
+				clueDisplayer.textContent = '';
+				const els = Array.from(cluesEl.getElementsByClassName('has-hover'));
+				els.filter(el2 => el2 !== el).forEach(el => el.classList.remove('has-hover'));
+				el.classList.remove('has-hover');
+			}
+		}
+
+		let previousClueSelection = null;
+
+		function isEquivalent(a, b) {
+		    var aProps = Object.getOwnPropertyNames(a);
+		    var bProps = Object.getOwnPropertyNames(b);
+
+		    if (aProps.length != bProps.length) {
+		        return false;
+		    }
+
+		    for (var i = 0; i < aProps.length; i++) {
+		        var propName = aProps[i];
+		        if (a[propName] !== b[propName]) {
+		            return false;
+		        }
+		    }
+
+		    return true;
+		}
+
+		function toggleClueSelection(clue) {
+			if(previousClueSelection !== null && isEquivalent(previousClueSelection, clue)) {
+				// unsetClue(clue.number, clue.direction, clue.answerLength);
+				//TODO: rem highlight!
+				blockHighlight = false;
+				previousClueSelection = null;
+				return;
+			}
+
+			blockHighlight = true;
+			previousClueSelection = clue;
+		}
+
+		//TODO: disable block highlight on grid click/different clue.
+		//TODO: handle click on li instead of span.
+
 		const onTap = function onTap(e) {
 			let target;
 			let clueDetails;
+			blockHighlight = false;
 
 			if(e.target.nodeName === 'TD' || e.target.nodeName === 'INPUT') {
 				target = e.target;
 			} else {
-				const num = e.target.parentElement.getAttribute('data-o-crossword-number');
+				const defEl = (e.target.nodeName === 'SPAN')?e.target.parentElement:e.target;
+
+				const num = defEl.getAttribute('data-o-crossword-number');
 				clueDetails = {};
 				clueDetails.number = num;
-				clueDetails.direction = e.target.parentElement.getAttribute('data-o-crossword-direction');
-				clueDetails.answerLength = e.target.parentElement.getAttribute('data-o-crossword-answer-length');
+				clueDetails.direction = defEl.getAttribute('data-o-crossword-direction');
+				clueDetails.answerLength = defEl.getAttribute('data-o-crossword-answer-length');
+
+				toggleClueSelection(clueDetails);
+				
 				const el = gridEl.querySelector(`td[data-o-crossword-number="${num}"]`);
 				target = el;
-
-				//TODO: lock direction
 			}
 
 			if (target === magicInput) {
@@ -542,6 +597,7 @@ OCrossword.prototype.assemble = function assemble() {
 
 				// iterate through list of answers associated with that cell
 				let index = clues.indexOf(currentlySelectedGridItem);
+				console.log('index::', index);
 
 				// if a new item is selected find what ever matches the current selection
 				if (index === -1 && currentlySelectedGridItem) {
@@ -555,7 +611,6 @@ OCrossword.prototype.assemble = function assemble() {
 				}
 
 				if (index !== -1 || !currentlySelectedGridItem) {
-
 					// the same cell has been clicked on again so
 					if (index + 1 === clues.length) index = -1;
 					currentlySelectedGridItem = clues[index + 1];
